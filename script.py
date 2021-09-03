98ec9b08a5b0fd1750b8cc62279893a5d9169133
RichardDavidsonTheGreat <rcdavids@ualberta.ca>
2021-09-03 12:16:38 -0600

    reworked script so that it downloads itself from GitHub and prints out its own source code from GitHub

--- script.py
+++ script.py
@@ -1,3 +1,6 @@
 import requests
-res = requests.get("http://www.google.com/")
+#res = requests.get("http://www.google.com/")
+#print(res.text)
+
+res = requests.get("https://raw.githubusercontent.com/RichardDavidsonTheGreat/404_Lab1/main/script.py")
 print(res.text)
